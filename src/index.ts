146f08c50094c5e85eb7b89e9c3afe55ec271aeb
ROBOT <tkata12@outlook.jp>
2025-08-04 16:19:34 +0900

    add: s func

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
+import type { StandardSchemaV1 } from "@standard-schema/spec";
+
+export const s = <T, U>(schema: StandardSchemaV1<T, U>) => ({
+  validate: (value: unknown) => schema["~standard"].validate(value)
+});
+
 export * from "./json-schema/index";
